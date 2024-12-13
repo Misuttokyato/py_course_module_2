1725386acdc972c0a811593fd2e33bba6e9c39d7
Максим Сергеев <21m.sergeev@gmail.com>
2024-12-14 02:28:10 +0600

    Update lab2_task2.py

--- Лабораторная работа 2/lab2_task2.py
+++ Лабораторная работа 2/lab2_task2.py
@@ -1,20 +1,12 @@
 BOOKS_DATABASE = [
-    {
-        "id": 1,
-        "name": "test_name_1",
-        "pages": 200,
-    },
-    {
-        "id": 2,
-        "name": "test_name_2",
-        "pages": 400,
-    }
+    {"id": 1, "name": "test_name_1", "pages": 200},
+    {"id": 2, "name": "test_name_2", "pages": 400}
 ]
 
 
 # TODO: написать класс Book
 class Book:
-    def __init__(self, id_, name, pages):
+    def init(self, id_, name, pages):
         """
         Инициализация книги.
 
@@ -26,14 +18,14 @@ class Book:
         self.name = name
         self.pages = pages
 
-    def __str__(self):
+    def str(self):
         """
         Возвращает строковое представление книги в виде:
         Книга "название_книги"
         """
         return f'Книга "{self.name}"'
 
-    def __repr__(self):
+    def repr(self):
         """
         Возвращает строку, которая может быть использована для инициализации такого же экземпляра:
         Book(id_=1, name='test_name_1', pages=200)
@@ -43,7 +35,7 @@ class Book:
 
 # TODO: написать класс Library
 class Library:
-    def __init__(self, books=None):
+    def init(self, books=None):
         """
         Инициализация библиотеки.
 
@@ -74,17 +66,18 @@ class Library:
         :raises ValueError: Если книги с запрашиваемым id не существует.
         """
         for i, book in enumerate(self.books):
-            if book['id'] == id_:
+            if book.id == id_:
                 return i
         raise ValueError('Книги с запрашиваемым id не существует')
 
 
-if __name__ == '__main__':
+if name == 'main':
     empty_library = Library()  # инициализируем пустую библиотеку
     print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
 
     list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"])
+        for book_dict in BOOKS_DATABASE
     ]
     library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
     print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
