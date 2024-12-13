b95df12c58c1be170ca019891ed8771079c21542
Максим Сергеев <21m.sergeev@gmail.com>
2024-12-14 01:38:02 +0600

    Update lab2_task2.py

--- Лабораторная работа 2/lab2_task2.py
+++ Лабораторная работа 2/lab2_task2.py
@@ -1,4 +1,15 @@
-BOOKS_DATABASE = [{"id": 1, "name": "test_name_1", "pages": 200, }, {"id": 2, "name": "test_name_2", "pages": 400, }]
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
 
 
 # TODO: написать класс Book
