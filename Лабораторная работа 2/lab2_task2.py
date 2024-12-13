cb0c8559ccc4840103d86b1309a581df00d9dad8
Максим Сергеев <21m.sergeev@gmail.com>
2024-12-14 00:54:58 +0600

    Update lab2_task2.py

--- Лабораторная работа 2/lab2_task2.py
+++ Лабораторная работа 2/lab2_task2.py
@@ -51,6 +51,7 @@ class Library:
         """
         if not self.books:
             return 1
+        # Возвращаем id последней книги + 1
         return self.books[-1].id + 1
 
     def get_index_by_book_id(self, id_):
@@ -71,9 +72,8 @@ if __name__ == '__main__':
     empty_library = Library()  # инициализируем пустую библиотеку
     print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
 
-    list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
-    ]
+    list_books = [Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in
+                  BOOKS_DATABASE]
     library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
     print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
 
