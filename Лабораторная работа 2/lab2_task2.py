8247da9665f3045e2eac1da15e9351ccc050a138
Максим Сергеев <21m.sergeev@gmail.com>
2024-12-14 00:40:43 +0600

    Не получилось с первого раза

--- Лабораторная работа 2/lab2_task2.py
+++ Лабораторная работа 2/lab2_task2.py
@@ -1,15 +1,4 @@
-BOOKS_DATABASE = [
-    {
-        "id": 1,
-        "name": "test_name_1",
-        "pages": 200,
-    },
-    {
-        "id": 2,
-        "name": "test_name_2",
-        "pages": 400,
-    }
-]
+BOOKS_DATABASE = [{"id": 1, "name": "test_name_1", "pages": 200, }, {"id": 2, "name": "test_name_2", "pages": 400, }]
 
 
 # TODO: написать класс Book
@@ -62,7 +51,6 @@ class Library:
         """
         if not self.books:
             return 1
-        # Возвращаем id последней книги + 1
         return self.books[-1].id + 1
 
     def get_index_by_book_id(self, id_):
@@ -83,9 +71,8 @@ if __name__ == '__main__':
     empty_library = Library()  # инициализируем пустую библиотеку
     print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
 
-    list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
-    ]
+    list_books = [Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in
+                  BOOKS_DATABASE]
     library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
     print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
 
