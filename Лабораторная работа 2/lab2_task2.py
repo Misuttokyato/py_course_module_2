2483f0a92217e3cfcf57adc83b088d00f60650b8
Максим Сергеев <21m.sergeev@gmail.com>
2024-12-14 00:58:13 +0600

    Update lab2_task2.py

--- Лабораторная работа 2/lab2_task2.py
+++ Лабораторная работа 2/lab2_task2.py
@@ -75,6 +75,6 @@ if __name__ == '__main__':
     list_books = [Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in
                   BOOKS_DATABASE]
     library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
-    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
+    print(library_with_books.get_next_book_id())  # проверяем следующий id для не пустой библиотеки
 
     print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
